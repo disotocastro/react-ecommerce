64a8699b082a32124e9a1e55521577aff7f50bcd
Diego Soto <juandisoca@gmail.com>
2022-09-29 13:58:22 -0600

    Configuración del proyecto 100% lista, se puede utilizar este commit como base

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-
-
-const App = () =>{
-  return(
-    <h1>Hola mundo</h1>
-  );
+const App = () => {
+	return (
+		<h1>Hola Mundo, Bienvenidos al Curso Practico de React!</h1>
+	);
 }
-
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,40 @@
-const path = requiere('path');
-const HtmlWebpackPlugin = requiere('html-webpack-plugin')
+const path = require('path'); //path del proyecto principal
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //traemos el plugin
+//de html
+
 module.exports = {
-  entry: './src/index.js',
-  // Punto de entrada de la app
-  output:{
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  resolve:{
-    extensions: ['.js','.jsx']
-    // reconoce elementos js y jsx
-  },
-  module:{
-    rules:[
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader'
-          }
+    entry: './src/index.js', // punto de entrada
+    output: { // lugar al que saldrán todos los archivos
+        path: path.resolve(__dirname, 'dist'), //en nuestro path, crea la carpeta dist
+        filename: 'bundle.js' // nombre del archivo js resultante
+    },
+    mode: 'development',
+    resolve: { // extensión de archivos a tomar en cuenta
+        extensions: ['.js', '.jsx']
+    },
+    module: { // loaders para cada tipo de archivo
+        rules: [ // reglas para usar
+            {
+                test: /\.(js|jsx)$/, // extensiones en las cuales actuará babel
+                exclude: /node_modules/, // siempre excluir node modules 
+                use: { // indicamos el loader
+                    loader: 'babel-loader' // babel 
+                }
+            },
+            {
+                test: /\.html$/, // extensiones html
+                use: [
+                    {
+                        loader: 'html-loader' // loader a usar
+                    }
+                ]
+            }
         ]
-      }
+    },
+    plugins: [ // plugins 
+        new HtmlWebpackPlugin({ // instanciamos el plugin para html 
+            template: './public/index.html', // archivo raíz a transformar
+            filename: './index.html' // el archivo resultante
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename:'./index.html'
-    })
-  ]
 }
