9981e19298e97041ca349c6f809ec40416b9a1a5
Diego Soto <juandisoca@gmail.com>
2022-10-03 17:18:39 -0600

    Sing up agregado

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -6,26 +6,27 @@ import RecoveryPassword from './../containers/RecoveryPassword';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './../pages/Home.jsx';
 import NotFound from './../pages/NotFound.jsx';
+import SingUp from './../containers/SingUp';
 
 
 
 const App = () => {
 	return (
-<BrowserRouter>
-	<Layout>
-		<Routes>
-			<Route exact path="/" element={<Home />} />
-			<Route exact path="/login" element={<Login />} />			
-			<Route exact path="/recovery-password" element={<RecoveryPassword />} />
-			<Route path="*" element={<NotFound />} />
-			{/* Archivo por default */}
-		</Routes>
-	</Layout>
-</BrowserRouter> 
+		<BrowserRouter>
+			<Layout>
+				<Routes>
+					<Route exact path="/" element={<Home />} />
+					<Route exact path="/login" element={<Login />} />
+					<Route exact path="/sing-up" element={<SingUp />} />
+					<Route exact path="/recovery-password" element={<RecoveryPassword />} />
+					<Route path="*" element={<NotFound />} />
+					{/* Archivo por default */}
+				</Routes>
+			</Layout>
+		</BrowserRouter>
 	);
 }
 
 export default App;
-
 // Layaout se abre y se cierra porque recibe {children}
 // login se cierra desde el principio porque no recibe ningun valor.
