f7df2310accf104a517236988aaa5db62b6d415e
Diego Soto <juandisoca@gmail.com>
2022-10-05 01:27:02 -0600

    Custom Hooks, tienda con imagenes al azar

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'; // Hook para trabajar con el estado
 import '../styles/ProductItem.scss';
 import add_to_cart from '@icons/bt_add_to_cart.svg';
 
-const ProductItem = () => {
+const ProductItem = ({ product }) => {
 	//estado y funcion modificadora
 	const [cart, setCart] = useState([]); // Valor inicial de Cart
 	const handleClick = () => {
@@ -11,11 +11,11 @@ const ProductItem = () => {
 
 	return (
 		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+			<img src={product.images[0]} alt={product.title} />
 			<div className="product-info">
 				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
+					<p>${product.price}</p>
+					<p>{product.title}</p>
 				</div>
 				<figure onClick={handleClick} >
 					<img src={add_to_cart} alt="" />
