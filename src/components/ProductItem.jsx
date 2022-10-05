c608e48edef99ace95e2a54785d13a324bf43db0
Diego Soto <juandisoca@gmail.com>
2022-10-05 16:39:03 -0600

    Carrito sigue sin funcionar, se le agrega un X para ver si el boton funciona

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,26 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import '@styles/ProductItem.scss';
 import AppContext from '../context/AppContext';
 import addToCartImage from '@icons/bt_add_to_cart.svg';
+import iconClose from '@icons/icon_close.png';
 
 const ProductItem = ({ product }) => {
-	const { addToCart } = useContext(AppContext);
+	const { addToCart, removeFromCart } = useContext(AppContext);
 
-	const handleClick = item => {
+
+
+	const [addedToCart, setAddedToCart] = useState(false);
+
+	const handleAdd = item => {
 		addToCart(item);
+		setAddedToCart(true);
+	}
+	const handleRemove = item => {
+		removeFromCart(item);
+		setAddedToCart(false);
 	}
 
 	return (
 		<div className="ProductItem">
-			<img src={product.images[0]} alt={product.title} />
+			<img src={product.images[1]} alt={product.title} />
 			<div className="product-info">
 				<div>
 					<p>${product.price}</p>
 					<p>{product.title}</p>
 				</div>
-				<figure onClick={() => handleClick(product)} >
-					<img src={addToCartImage} alt="" />
-				</figure>
+				{!addedToCart ?
+					<figure onClick={() => handleAdd(product)} >
+						<img src={addToCartImage} alt="" />
+					</figure>
+					:
+					<figure onClick={() => handleRemove(product)} >
+						<img src={iconClose} alt="" />
+					</figure>
+				}
 			</div>
 		</div>
 	);
--- src/hooks/useInitialState.js
+++ src/hooks/useInitialState.js
@@ -8,16 +8,25 @@ const useInitialState = () => {
   const [state, setState] = useState(initialState);
 
   const addToCart = (payload) => {
+    if (!state.cart.includes(payload)) {
+      setState({
+        ...state,
+        cart: [...state.cart, payload]
+      });
+    }
+  };
+  const removeFromCart = (payload) => {
+    const newArray = state.cart.filter(product => product != payload);
     setState({
       ...state,
-      // ... sirve para mantener items anteriores
-      cart: [...state.cart, payload]
+      cart: [...newArray]
     });
-  };
 
+  }
   return {
     state,
-    addToCart
+    addToCart,
+    removeFromCart
   }
 }
 
