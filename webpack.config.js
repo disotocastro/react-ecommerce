5b6b79faf3b3b262a88fb0e25445758942de967d
Diego Soto <juandisoca@gmail.com>
2022-10-10 14:23:54 -0600

    FIX GH  Actions 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path'); //path del proyecto principal
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //traemos el plugin//de html
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-        publicPath: "./", //! para el despliegue lo importante es el punto
+    entry: './src/index.js', // punto de entrada
+    output: { // lugar al que saldrán todos los archivos
+        path: path.resolve(__dirname, 'dist'), //en nuestro path, crea la carpeta dist
+        filename: 'bundle.js', // nombre del archivo js resultante
+        publicPath: '/'
     },
     mode: 'development',
-    resolve: {
-        extensions: [".js", ".jsx"],
+    resolve: { // extensión de archivos a tomar en cuenta
+        extensions: ['.js', '.jsx'],
         alias: {
             '@components': path.resolve(__dirname, 'src/components/'),
             '@containers': path.resolve(__dirname, 'src/containers/'),
@@ -20,49 +21,45 @@ module.exports = {
             '@styles': path.resolve(__dirname, 'src/styles/'),
             '@icons': path.resolve(__dirname, 'src/assets/icons/'),
             '@logos': path.resolve(__dirname, 'src/assets/logos/'),
-        },
+        }
     },
-    module: {
-        rules: [
+    module: { // loaders para cada tipo de archivo
+        rules: [ // reglas para usar
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                },
+                test: /\.(js|jsx)$/, // extensiones en las cuales actuará babel
+                exclude: /node_modules/, // siempre excluir node modules 
+                use: { // indicamos el loader
+                    loader: 'babel-loader' // babel 
+                }
             },
             {
-                test: /\.(png|jpg|svg|jpeg|web|svg)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: "public/[hash][ext]",
-                },
-            },
-            {
-                test: /\.html$/,
+                test: /\.html$/, // extensiones html
                 use: [
                     {
-                        loader: "html-loader",
-                    },
-                ],
-            },
-            {
-                test: /\.(css|scss)$/,
+                        loader: 'html-loader' // loader a usar
+                    }
+                ]
+            }, {
+                test: /\.(css|s[ac]ss)$/i,
                 use: [
                     "style-loader",
                     "css-loader",
                     "sass-loader",
-                ],
+                ]
             },
-        ],
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                type: 'asset'
+            }
+        ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-            filename: "./index.html",
+    plugins: [ // plugins 
+        new HtmlWebpackPlugin({ // instanciamos el plugin para html 
+            template: './public/index.html', // archivo raíz a transformar
+            filename: './index.html' // el archivo resultante
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].css',
+            filename: '[name].css'
         }),
     ],
     devServer: {
@@ -70,7 +67,7 @@ module.exports = {
             directory: path.join(__dirname, 'public'),
         },
         compress: true,
-        port: 64340,
-        historyApiFallback: true,
+        port: 3005,
+        historyApiFallback: true
     }
 }
