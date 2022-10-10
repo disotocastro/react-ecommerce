331c8a0394115e7ddf09f162ccdcedcc55e587a9
Diego Soto <juandisoca@gmail.com>
2022-10-10 14:15:08 -0600

    ADD GitHub Actions

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import { createRoot } from 'react-dom/client';
 
 // ReactDOM.render(<App />, document.getElementById('app'));
 
-
-
-
 const root = createRoot(document.querySelector('#app'));
 root.render(<App />);
 
